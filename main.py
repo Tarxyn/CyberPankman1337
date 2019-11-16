3f02ae89dbded0d3b732fe2c15f9077d80daaccf
rugrisser <rugrisser@gmail.com>
2019-11-16 19:17:20 +0300

    PyGame init

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import pygame
+import sys
+
 def main():
 
-    pass
+    size = width, height = 800, 600
+    black = 0, 0, 0
+
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    gameover = False
+
+    while not gameover:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameover = True
+
+        screen.fill(black)
+
+        pygame.display.flip()
+        pygame.time.wait(5)
+
+    sys.exit()
 
 if __name__ == '__main__':
     main()
